b6649e4e5710c2225c1ff7461cd82685b616931c
Edward Dale <scompt@scompt.com>
2021-12-13 14:47:32 +0100

    Day 01, Part 2

--- day_01/main.rs
+++ day_01/main.rs
@@ -4,13 +4,16 @@ fn main() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let mut level = 0;
-        for c in line.unwrap().chars() {
+        for (i, c) in line.unwrap().chars().enumerate() {
             if c == '(' {
                 level += 1
             } else if c == ')' {
                 level -= 1
             }
+            if level < 0 {
+                println!("{}", i+1);
+                return;
+            }
         }
-        println!("{}", level);
     }
 }
