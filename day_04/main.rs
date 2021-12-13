de39ae40e7818223865d81b8ea3f3cec28dfceb8
Edward Dale <scompt@scompt.com>
2021-12-13 20:40:49 +0100

    Day 04, Part 1

---
+++ day_04/main.rs
@@ -0,0 +1,17 @@
+use md5;
+
+fn main() {
+    let secret_key = "yzbqklnj";
+
+    let mut index = 0;
+    loop {
+        index += 1;
+        let input = format!("{}{}", secret_key, index);
+        let digest = md5::compute(input.as_bytes());
+
+        if digest[0] == 0 && digest[1] == 0 && digest[2] < 16 {
+            println!("{}", index);
+            return;
+        }
+    }
+}
